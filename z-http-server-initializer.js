772379eee0a7102e4e219b6d4ccb86d4caa58d0d
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-10-17 19:59:04 +0400

    init

---
+++ z-http-server-initializer.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = {
+    run: function (app, next) {
+        var http = require('http');
+
+        /**
+         * Create HTTP server.
+         */
+        var server = http.createServer(app.express);
+
+        /**
+         * Listen on provided port, on all network interfaces.
+         */
+        server.listen(app.config.zApp.port);
+
+        server.on('error', onError);
+        server.on('listening', onListening);
+
+        /**
+         * Event listener for HTTP server "error" event.
+         */
+        function onError(error) {
+            if (error.syscall !== 'listen') {
+                throw error;
+            }
+
+            var bind = typeof app.config.zApp.port === 'string'
+                ? 'Pipe ' + app.config.zApp.port
+                : 'Port ' + app.config.zApp.port;
+
+            // handle specific listen errors with friendly messages
+            switch (error.code) {
+                case 'EACCES':
+                    app.logger.warn('requires elevated privileges', bind);
+                    process.exit(1);
+                    break;
+                case 'EADDRINUSE':
+                    app.logger.warn('port already in use', bind);
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        }
+
+        /**
+         * Event listener for HTTP server "listening" event.
+         */
+        function onListening() {
+            var addr = server.address();
+            var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+            app.logger.info('server started listening on', bind);
+        }
+
+        /**
+         * Event listener for HTTP server "uncaught exception" event handler.
+         */
+        process.on('uncaughtException', function (error) {
+            console.log("!!!!!!!!!!!!!!!!!! UNHANDLED EXCEPTION !!!!!!!!!!!!!!!!!!");
+            app.logger.warn(error.stack);
+            console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        });
+
+        next();
+    }
+};
